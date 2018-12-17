ee39a8da7ceb98a1ae1b450c8fea27b4419a4985
James Jackson <James@Grandcircus.co>
2018-12-16 21:36:34 -0500

    James TA Review

--- Lab18/Program.cs
+++ Lab18/Program.cs
@@ -1,77 +1,78 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 
 namespace LinkedListLab
 {
-    public static class LinkedListExtensions
-    {
+	//James -- I would put this in it's own file 
+	public static class LinkedListExtensions
+	{
 
-        public static bool RemoveAt<T>(this LinkedList<T> list, int number)
-        {
-            var index2 = number - 1;
+		public static bool RemoveAt<T>(this LinkedList<T> list, int number)
+		{
+			var index2 = number - 1;
 
-            LinkedListNode<T> NodeOne = list.First;
-            for (int i = 0; i <= index2 && NodeOne != null; i++)
-            {
-                if (i != index2)
-                {
-                     var Node = NodeOne.Next; 
-                    continue;
-                }
-                list.Remove(NodeOne);
-            }
-            return false; 
-        }
+			LinkedListNode<T> NodeOne = list.First;
+			for (int i = 0; i <= index2 && NodeOne != null; i++)
+			{
+				if (i != index2)
+				{
+					var Node = NodeOne.Next;
+					continue;
+				}
+				list.Remove(NodeOne);
+			}
+			return false;
+		}
 
-        public static void PrintReverse<T>(this LinkedList<T> list)
-        {
-            for (int j = list.Count - 1; j >= 0; j--)
-            {
-                Console.WriteLine(list.ElementAt(j));
-            }
-        }
+		public static void PrintReverse<T>(this LinkedList<T> list)
+		{
+			for (int j = list.Count - 1; j >= 0; j--)
+			{
+				Console.WriteLine(list.ElementAt(j));
+			}
+		}
 
-        public static bool  InsertAt<T>(this LinkedList<T> list, int index, T o)
-        {
-            return false; 
-        }
-    }
-
-
-    public static class StringExtension
-    {
-        public static string ToCamelCase(this string value)
-        {
-            return string.Empty;
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            int[] numbers = new int[] { 1, 1, 1, 2, 4, 7, 7, 8, 9, 5 };
-            var newlist = new LinkedList<int>(numbers);
+		// James  -- Not inserting anything here
+		public static bool InsertAt<T>(this LinkedList<T> list, int index, T o)
+		{
+			return false;
+		}
+	}
 
 
+	public static class StringExtension
+	{
+		// James - This does not return anything to camel case sir
+		public static string ToCamelCase(this string value)
+		{
+			return string.Empty;
+		}
+	}
 
-            Console.WriteLine("list in reverse order");
-            newlist.PrintReverse();
-            Console.WriteLine();
-            Console.ReadLine();
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = new int[] { 1, 1, 1, 2, 4, 7, 7, 8, 9, 5 };
+			var newlist = new LinkedList<int>(numbers);
 
 
-            Console.WriteLine("Please suggest a position in the array to remove");
-            Console.ReadLine(); 
-            newlist.RemoveAt(2);
-            Console.WriteLine();
-            Console.ReadLine();
+
+			Console.WriteLine("list in reverse order");
+			newlist.PrintReverse();
+			Console.WriteLine();
+			Console.ReadLine();
 
 
-        }
-    }
+			Console.WriteLine("Please suggest a position in the array to remove");
+			Console.ReadLine();
+			newlist.RemoveAt(2);
+			Console.WriteLine();
+			Console.ReadLine();
+
+
+		}
+	}
 }
